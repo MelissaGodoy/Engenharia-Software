9b5edfdaa6a6fac2ced2b489f7032aebd4407182
joseguilherme96 <jose.guilherme96@outlook.com.br>
2023-05-07 22:28:36 -0300

    Tabela Emprestimo

--- sql.sql
+++ sql.sql
@@ -16,3 +16,14 @@ NomeAmigo varchar(100) not null)
 
 alter table CadastroAmigo 
 Add Constraint Pk_CadastroAmigo_IdAmigo Primary key(IdAmigo)
+
+CREATE TABLE emprestimo(
+	IdEmprestimo int Identity(1,1), 
+	IdAmigo int,
+	CodigoDaRevista Int,
+	DataEmprestimo date Not Null,
+	DataDevolucao date Not Null,
+)
+
+alter table emprestimo
+	add constraint PK_emprestimo_IdEmprestimo Primary Key(IdEmprestimo)
