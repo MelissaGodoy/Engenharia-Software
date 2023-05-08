c9e24dd4f65f2d5cd3b8e7cd1be7a8eb23d951b9
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2023-05-07 22:16:11 -0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -6,3 +6,13 @@ Go
 --Conectando-se ao banco de dados--
 Use EngenhariaSoftware
 Go
+
+create table CadastroAmigo
+(IdAmigo int Not Null Identity(1,1),
+Endereco varchar(100) null,
+Telefone int null,
+NomeResponsavel varchar(100) null,
+NomeAmigo varchar(100) null)
+
+alter table CadastroAmigo 
+Add Constraint Pk_IdAmigo Primary key(IdAmigo)
